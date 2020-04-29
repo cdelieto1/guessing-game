01859b07e10a4ec1c631843ce067935823dceecf
cassandra delieto <cassandradelieto@gmail.com>
2020-04-29 15:29:32 +0000

    Initial commit for the guessing game

---
+++ game.py
@@ -0,0 +1,31 @@
+"""A number-guessing game."""
+import time
+
+# Put your code here
+from random import *
+
+fname = input("What's ya name?: ")
+print(f"{fname}, I'm thinking of a number between 1 and 100.") 
+
+random_number=randint(1,100)
+guess = int(input("Try to guess the numnber: "))
+
+def guesstimaton(random_number):
+
+	print('Drumroll....')
+	time.sleep(2)
+	print('... still thinking...')
+	time.sleep(2)
+
+	if guess > 100 or guess < 0:
+		return "Number needs to be between 0 and 100, smartass!"
+	elif guess==random_number:
+		return "bingo!"
+	elif random_number < guess:
+		return "You're higher than the guess number"
+	elif random_number > guess:
+		return "You're lower than the guess number"
+	else:
+		return "Try playing again."
+
+print(guesstimaton(random_number))		
